aaa11ccb52dd494de984ceb496f75ebcc46a0dc2
SD <namelosted@gmail.com>
2018-09-20 22:43:58 +0300

    rename module add help code refactoring remove unused code

--- index.js
+++ index.js
@@ -1,226 +1,119 @@
-module.exports = function(RED) {
-    "use strict";
-
-    function ClickHoldProcessorLogic(config) {
-        RED.nodes.createNode(this, config);
-
-        var node = this;
-        //var config = config_;
-
-        node.on('input', function(msg) {
-            const isPushEvent = (msg.payload.state === 'push');
-
-            function ClearRepeater(what) {
-                const holdRepeatTimer = node[what];
-                if (holdRepeatTimer) {
-                    delete node[what];
-                    clearInterval(holdRepeatTimer);
-                }
-            }
-            
-            function ClearHoldTimer() {
-                const holdTimer = node.holdTimer;
-                if (holdTimer) {
-                    delete node.holdTimer;
-                    clearTimeout(holdTimer);
-                }
-            }
-    
-            function ResetGuardWatchdog() {
-                const guard = node.HoldGuard;
-    
-                if (guard) {
-                    ClearHoldTimer();
-                    ClearRepeater('holdRepeatTimer');
-                    ClearRepeater('holdGuardTimer');
-                    
-                    delete node.button_state;
-                    
-                    msg.event = 'release';
-                    node.send(msg);
-                    
-                    node.status({fill:'red',shape:'dot',text:"hold not confirmed!"});
-                }
-                else
-                    node.HoldGuard = true;
-            }
-
-
-            if (isPushEvent) {
-                if (msg.payload.repeat) {
-                    node.HoldGuard = false;
-                    return;
-                }
-            
-                { // validate
-                    const holdTimer = node.holdTimer;
-                    const holdRepeatTimer = node.holdRepeatTimer;
-                    if (holdTimer || holdRepeatTimer) {
-                        node.error('\'PUSH\' event was twice received! Skip it.');
-                        node.status({fill:'red'});
-                        return;
-                    }
-                }
-            
-                node.status({fill:'green',shape:'ring',text:"push"});
-                node.button_state = 'click';
-            
-                
-                const holdTimer = setTimeout(function() {
-                    node.status({fill:'green',shape:'dot',text:"hold"});
-                    
-                    node.button_state = 'release';
-                    delete node.holdTimer;
-                    
-                    msg.event = 'press';
-                    msg.repeat = 0;
-                    
-                    node.send(msg);
-                    
-                    const holdRepeatTimer = setInterval(function() {
-                        ++msg.repeat;
-                        node.send(msg);
-                    }, config.holdRepeatInterval);
-                    node.holdRepeatTimer = holdRepeatTimer;
-                    
-                    if (config.holdRepeatGuard) {
-                        const holdGuardTimer = setInterval(ResetGuardWatchdog, config.holdRepeatGuard);
-                        node.holdGuardTimer = holdGuardTimer;
-                    }
-                }, config.holdTimeout);
-
-                node.holdTimer = holdTimer;
-            }
-            else {
-                node.status({fill:'grey', text:"standby"});
-                
-                msg.event = node.button_state;
-                delete node.button_state;
-                { // clear timers
-                    ClearHoldTimer();
-                    ClearRepeater('holdRepeatTimer');
-                    ClearRepeater('holdGuardTimer');
-                }
-            }
-
-            msg.buttonId = config.buttonId;
-
-            if (msg.event)
-                node.send(msg);
-        });
-    }
-
-    RED.nodes.registerType("sd-click-hold-processor", ClickHoldProcessorLogic);
-}
-
-
-/*const HoldTimeout           = 350; //ms
-const HoldRepeatInterval    = 800; //ms
-const HoldRepeatGuard       = 800; //ms*/
-
-
-/*
-Events:
-1. click
-2. press(repeat:0)...press(repeat:1)...release
-*/
-
-
-/*const isPushEvent = (msg.payload.state === 'push');
-
+"use strict";
 
 function ClearRepeater(what) {
-    const holdRepeatTimer = context.get(what);
+    const holdRepeatTimer = this[what];
+
     if (holdRepeatTimer) {
-        context.set(what, null);
+        delete this[what];
         clearInterval(holdRepeatTimer);
-    }  
+    }
 }
 
 function ClearHoldTimer() {
-    const holdTimer = context.get('holdTimer');
+    const holdTimer = this.holdTimer;
+
     if (holdTimer) {
-        context.set('holdTimer', null);
+        delete this.holdTimer;
         clearTimeout(holdTimer);
     }
 }
 
-function ResetGuardWatchdog() {
-    const guard = context.get('HoldGuard');
-//node.warn('ResetGuardWatchdog: ' + guard);
-    if (guard) {
-        ClearHoldTimer();
-        ClearRepeater('holdRepeatTimer');
-        ClearRepeater('holdGuardTimer');
-        
-        context.set('button_state', null);
-        
-        msg.event = 'release';
-        node.send(msg);
-        
-        node.status({fill:'red',shape:'dot',text:"hold not confirmed!"});
+module.exports = function(RED) {
+    function ClickHoldProcessorLogic(config) {
+        RED.nodes.createNode(this, config);
+
+        this.on('input', function(msg) {
+            const isPushEvent = (msg.payload.state === 'push');
+
+            if (isPushEvent) {
+                if (msg.payload.repeat) {
+                    this.HoldGuard = false;
+                    return;
+                }
+            
+                { // validate
+                    const holdTimer = this.holdTimer;
+                    const holdRepeatTimer = this.holdRepeatTimer;
+                    if (holdTimer || holdRepeatTimer) {
+                        this.error('\'PUSH\' event was twice received! Skip it.');
+                        this.status({fill:'red'});
+                        return;
+                    }
+                }
+            
+                this.status({fill:'green',shape:'ring',text:"push"});
+                this.button_state = 'click';
+            
+                
+                const holdTimer = setTimeout(() => {
+                    this.status({fill:'green',shape:'dot',text:"hold"});
+                    
+                    this.button_state = 'release';
+                    delete this.holdTimer;
+                    
+                    msg.event = 'press';
+                    msg.repeat = 0;
+                    
+                    this.send(msg);
+                    
+                    const holdRepeatTimer = setInterval(() => {
+                        ++msg.repeat;
+                        this.send(msg);
+                    }, config.holdRepeatInterval);
+                    this.holdRepeatTimer = holdRepeatTimer;
+                    
+                    if (config.holdRepeatGuard) {
+                        function ResetGuardWatchdog() {
+                            const guard = this.HoldGuard;
+                    
+                            if (guard) {
+                                this.ClearAllTimers();
+                                delete this.button_state;
+                                
+                                msg.event = 'release';
+                                this.send(msg);
+                                
+                                this.status({fill:'red', shape:'dot', text:"hold not confirmed!"});
+                            }
+                            else
+                                this.HoldGuard = true;
+                        }
+
+                        this.holdGuardTimer = setInterval(ResetGuardWatchdog.bind(this), config.holdRepeatGuard);
+                    }
+                }, config.holdTimeout);
+
+                this.holdTimer = holdTimer;
+            }
+            else {
+                this.status({fill:'grey', text:"standby"});
+                
+                msg.event = this.button_state;
+                
+                this.ClearAllTimers();
+                delete this.button_state;
+            }
+
+            if (msg.event)
+                this.send(msg);
+        });
+
+        this.on('close', function(done) {
+            this.ClearAllTimers();
+
+            delete this.button_state;
+            this.status({fill:'grey', text:"standby"});
+
+            done();
+        });
+
+
+        this.ClearAllTimers = function() {
+            ClearHoldTimer.call(this);
+            ClearRepeater.call(this, 'holdRepeatTimer');
+            ClearRepeater.call(this, 'holdGuardTimer');
+        };
     }
-    else
-        context.set('HoldGuard', true);
+
+    RED.nodes.registerType("Click-Hold logic", ClickHoldProcessorLogic);
 }
-
-
-if (isPushEvent) {
-    if (msg.payload.repeat) {
-        context.set('HoldGuard', null);
-        return;
-    }
-
-    { // validate
-        const holdTimer = context.get('holdTimer');
-        const holdRepeatTimer = context.get('holdRepeatTimer');
-        if (holdTimer || holdRepeatTimer) {
-            node.error('\'PUSH\' event was twice received! Skip it.');
-            node.status({fill:'red'});
-            return;
-        }
-    }
-
-    node.status({fill:'green',shape:'ring',text:"push"});
-    context.set('button_state', 'click');
-
-    
-    const holdTimer = setTimeout(function() {
-        node.status({fill:'green',shape:'dot',text:"hold"});
-        
-        context.set('button_state', 'release');
-        context.set('holdTimer', null);
-        
-        msg.event = 'press';
-        msg.repeat = 0;
-        
-        node.send(msg);
-        
-        const holdRepeatTimer = setInterval(function() {
-            ++msg.repeat;
-            node.send(msg);
-        }, HoldRepeatInterval);
-        context.set('holdRepeatTimer', holdRepeatTimer);
-        
-        if (HoldRepeatGuard) {
-            const holdGuardTimer = setInterval(ResetGuardWatchdog, HoldRepeatGuard);
-            context.set('holdGuardTimer', holdGuardTimer);
-        }
-    }, HoldTimeout);
-    context.set('holdTimer', holdTimer);
-}
-else {
-    node.status({fill:'grey', text:"standby"});
-    
-    msg.event = context.get('button_state');
-    context.set('button_state', null);
-    { // clear timers
-        ClearHoldTimer();
-        ClearRepeater('holdRepeatTimer');
-        ClearRepeater('holdGuardTimer');
-    }
-}
-
-if (msg.event)
-    return msg;
-*/
